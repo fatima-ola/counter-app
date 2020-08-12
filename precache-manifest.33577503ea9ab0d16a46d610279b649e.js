99dbd050024ffb91bf9e90d8ddc41b6444212554
MyMedicalBank <software@mymedicalbank.com>
2020-08-12 01:42:02 +0100

    Updates

---
+++ precache-manifest.33577503ea9ab0d16a46d610279b649e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055b0e3f0d573d54af4970195eb3abcf",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "4a9f910b56b19547c06a",
+    "url": "/counter-app/static/css/main.5c365426.chunk.css"
+  },
+  {
+    "revision": "f64be389cd0a33cb8f12",
+    "url": "/counter-app/static/js/2.b50dd724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-app/static/js/2.b50dd724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9f910b56b19547c06a",
+    "url": "/counter-app/static/js/main.d292ebc2.chunk.js"
+  },
+  {
+    "revision": "8d3c91def09e322d68af",
+    "url": "/counter-app/static/js/runtime-main.7c4e8537.js"
+  }
+]);
